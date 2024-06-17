0065bd8377ff853f9dd887919081c4bb6785cf96
Raghav <raghav4u03@gmail.com>
2024-06-17 20:16:36 +0530

    managed sessions

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from .models import Room
 from .serializers import RoomSerializer, CreateRoomSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 # we will create all the endpoints here --- 
 
@@ -76,4 +77,15 @@ class JoinRoom(APIView):
         return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status = status.HTTP_400_BAD_REQUEST)
         
 
-  
+class UserInRoom(APIView):
+
+    def get(self, request, format = None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+
+        return JsonResponse(data, status = status.HTTP_200_OK)
+
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect,
-  Routes,
-} from "react-router-dom";
+import { Grid, Button, ButtonGroup, Typography } from "@mui/material";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+// import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export default function HomePage() {
-  const homePageContent = () => {};
+  const [roomCode, setRoomCode] = useState(null);
+  // const navigate = useNavigate();
+  useEffect(() => {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        setRoomCode(data.code);
+      });
+  }, []);
+
+  const homePageContent = () => {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            House Party
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Link to="/join">
+              <Button color="primary">Join a Room</Button>
+            </Link>
+            <Link to="/create">
+              <Button color="secondary">Create a Room</Button>
+            </Link>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={homePageContent} />
+        <Route
+          path="/"
+          element={
+            roomCode ? <Navigate to={`/room/${roomCode}`} /> : homePageContent()
+          }
+        />
         <Route path="/join" element={<RoomJoinPage />} />
         <Route path="/create" element={<CreateRoomPage />} />
         <Route path="/room/:roomCode" element={<Room />} />
